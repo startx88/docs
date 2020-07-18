2b3daab5ac9b0f45d0a159fb706b5131e6f5d993
startx88 <arya.creativemind@gmail.com>
2020-07-18 21:31:49 +0530

    add nxinx

---
+++ nginx/intro.js
@@ -0,0 +1,15 @@
+/**
+ * What is nginx:
+ * 1. It is free,
+ * 2. Open source
+ * 3. HTTP Web Server,
+ * 4. Mail proxy server,
+ * 5. Reverse proxy
+ * 6. Load Balancer
+ *    for HTTP, TCP and URP traffic.
+ *
+ *
+ * It is known for its high performance, stability,
+ * rich featre set, simple configuration and low resource
+ * consumption
+ */
